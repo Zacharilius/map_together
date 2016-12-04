7ff58010cab9bc66005b0fabc6879664aa63c877
Zachary A Bensley <zabensley@gmail.com>
2016-12-04 15:40:02 -0800

    Nav: Uses a Gravatar image for the navbar Avatar.

--- map_together/util.py
+++ map_together/util.py
@@ -6,7 +6,14 @@ from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST
 from django.utils.safestring import mark_safe
 import json
+import hashlib
+import urllib
 
+
+def gravatar_url(email, size=40):
+    default = "retro"
+    return "https://www.gravatar.com/avatar/%s?%s" % (hashlib.md5(email.lower().encode('utf-8')).hexdigest(), urllib.parse.urlencode({'d':default, 's':str(size)}))
+ 
 
 def generate_nav_info_for_user(user):
     if user.is_authenticated():
@@ -23,12 +30,15 @@ def generate_nav_info_for_user(user):
     return user_info
 
 
-
 def generate_nav_info(user):
-    return dict(
-            username=user.username,
-            is_logged_in=user.is_authenticated(),
-            profile_url=reverse('profile'),
-            login_url=reverse('login'),
-            logout_url=reverse('logout'),
-        )
+    if user.is_authenticated():
+        return dict(
+                username=user.username,
+                gravatar_url=gravatar_url(user.email),
+                is_logged_in=user.is_authenticated(),
+            )
+    else:
+        return dict(
+                username=user.username,
+                is_logged_in=user.is_authenticated(),
+            )
