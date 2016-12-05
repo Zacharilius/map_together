b48999ba0daf36be1b03402f9026148004104717
Zachary A Bensley <zabensley@gmail.com>
2016-12-04 19:26:21 -0800

    Map Room: Refactored Channels consumers and routing.

--- map_room/consumers.py
+++ map_room/consumers.py
@@ -1,72 +1,76 @@
 from channels import Group
 from channels.auth import http_session_user, channel_session_user, channel_session_user_from_http
+from channels.generic.websockets import WebsocketConsumer
 from channels.sessions import channel_session
 from django.utils.safestring import mark_safe
 import json
 from .models import MapRoom, ChatMessage
 
 
-@channel_session
-def ws_map_sync_connect(message, map_room):
-    print('==== ws_map_sync_connect ====')
+class MapSync(WebsocketConsumer):
+    http_user = True
+    channel_session_user = True
 
-    map_room = MapRoom.objects.get(label=map_room)
-    Group('map_room-sync-' + map_room.label).add(message.reply_channel)
-    message.channel_session['map_room'] = map_room.label
+    def connect(self, message, **kwargs):
+        print('==== ws_map_sync_connect ====')
 
-
-@channel_session
-def ws_map_sync_receive(message, map_room):
-    print('==== ws_map_sync_receive ====')
-    
-    label = message.channel_session['map_room']
-    map_room = MapRoom.objects.get(label=label)
-    data = json.loads(message['text'])
-    
-    map_room.center_lng = data['mapCenter']['lng']
-    map_room.center_lat = data['mapCenter']['lat']
-    map_room.zoom_level = data['zoomLevel']
-    map_room.save()
-    
-    Group('map_room-sync-'+label).send({'text': message['text']})
+        map_room = kwargs['map_room']
+        map_room = MapRoom.objects.get(label=map_room)
+        Group('map_room-sync-' + map_room.label).add(message.reply_channel)
+        message.channel_session['map_room'] = map_room.label
 
 
-@channel_session
-def ws_map_sync_disconnect(message, map_room):
-    print('==== ws_map_sync_disconnect ====')
-    
-    label = message.channel_session['map_room']
-    Group('map_room-sync-'+label).discard(message.reply_channel)
+    def receive(self, text, **kwargs):
+        print('==== ws_map_sync_receive ====')
+        
+        map_room = kwargs['map_room']
+        label = self.message.channel_session['map_room']
+        map_room = MapRoom.objects.get(label=label)
+        data = json.loads(text)
+        
+        map_room.center_lng = data['mapCenter']['lng']
+        map_room.center_lat = data['mapCenter']['lat']
+        map_room.zoom_level = data['zoomLevel']
+        map_room.save()
+        
+        Group('map_room-sync-'+label).send({'text': text})
 
 
-@channel_session_user_from_http
-def ws_chat_connect(message, map_room):
-    print('==== ws_chat_connect ====')
-
-    map_room = MapRoom.objects.get(label=map_room)
-    Group('map_room-chat-' + map_room.label).add(message.reply_channel)
-    message.channel_session['map_room'] = map_room.label
+    def disconnect(self, message, **kwargs):
+        print('==== ws_map_sync_disconnect ====')
+        
+        label = message.channel_session['map_room']
+        Group('map_room-sync-'+label).discard(message.reply_channel)
 
 
-@channel_session_user
-def ws_chat_receive(message, map_room):
-    print('==== ws_chat_receive ====')
-    
-    label = message.channel_session['map_room']
-    map_room = MapRoom.objects.get(label=label)
-    
-    data = json.loads(message['text'])
-    message_text = data['messageText']
-    chat_message = ChatMessage(owner=message.user, map_room=map_room, message=message_text)
-    chat_message.save()
-    
-    Group('map_room-chat-'+label).send({'text': json.dumps(chat_message.format_message_info())})
+class Chat(WebsocketConsumer):
+    http_user = True
+    channel_session_user = True
+
+    def connect(self, message, **kwargs):
+        print('==== ws_chat_connect ====')
+
+        map_room = kwargs['map_room']
+        map_room = MapRoom.objects.get(label=map_room)
+        Group('map_room-chat-' + map_room.label).add(message.reply_channel)
+        message.channel_session['map_room'] = map_room.label
 
 
-@channel_session
-def ws_chat_disconnect(message, map_room):
-    print('==== ws_chat_disconnect ====')
-    
-    label = message.channel_session['map_room']
-    Group('map_room-chat-'+label).discard(message.reply_channel)
+    def receive(self, text, **kwargs):
+        print('==== ws_chat_receive ====')
+
+        label = self.message.channel_session['map_room']
+        map_room = MapRoom.objects.get(label=label)
+        message = json.loads(text)
+        chat_message = ChatMessage(owner=self.message.user, map_room=map_room, message=message['messageText'])
+        chat_message.save()
+        
+        Group('map_room-chat-'+label).send({'text': json.dumps(chat_message.format_message_info())})
+
+
+    def disconnect(self, message, **kwargs):
+        print('==== ws_chat_disconnect ====')
+        
+        label = message.channel_session['map_room']
+        Group('map_room-chat-'+label).discard(message.reply_channel)
 
--- map_room/routing.py
+++ map_room/routing.py
@@ -1,6 +1,6 @@
-from channels.routing import route
+from channels.routing import route, route_class
 from channels.staticfiles import StaticFilesConsumer
-from .consumers import *
+from . import consumers
 
 
 MAP_ROOM_ROUTE = r'^/ws/map-room/room/(?:(?P<map_room>[a-zA-Z0-9_-]+))/map-sync$'
@@ -10,12 +10,14 @@ channel_routing = [
     route('http.request', StaticFilesConsumer()),
 	      
     # Map Sync
-    route("websocket.connect", ws_map_sync_connect, path=MAP_ROOM_ROUTE),
-    route("websocket.receive", ws_map_sync_receive, path=MAP_ROOM_ROUTE),
-    route("websocket.disconnect", ws_map_sync_disconnect, path=MAP_ROOM_ROUTE),
+    route_class(consumers.MapSync, path=r'^/ws/map-room/room/(?:(?P<map_room>[a-zA-Z0-9_-]+))/map-sync$'),
+    # route("websocket.connect", ws_map_sync_connect, path=MAP_ROOM_ROUTE),
+    # route("websocket.receive", ws_map_sync_receive, path=MAP_ROOM_ROUTE),
+    # route("websocket.disconnect", ws_map_sync_disconnect, path=MAP_ROOM_ROUTE),
     
     # Chat
-    route("websocket.connect", ws_chat_connect, path=MAP_ROOM_CHAT),
-    route("websocket.receive", ws_chat_receive, path=MAP_ROOM_CHAT),
-    route("websocket.disconnect", ws_chat_disconnect, path=MAP_ROOM_CHAT),
+    route_class(consumers.Chat, path=r'^/ws/map-room/room/(?:(?P<map_room>[a-zA-Z0-9_-]+))/chat$'),
+    # route("websocket.connect", ws_chat_connect, path=MAP_ROOM_CHAT),
+    # route("websocket.receive", ws_chat_receive, path=MAP_ROOM_CHAT),
+    # route("websocket.disconnect", ws_chat_disconnect, path=MAP_ROOM_CHAT),
 ]
